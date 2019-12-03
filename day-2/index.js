8871bd32f6df028674b4e9aa59edb8db485565c0
Evan Charlton <evancharlton@gmail.com>
2019-12-03 17:38:19 +0100

    Day 2 (part 2)

--- day-2/index.js
+++ day-2/index.js
@@ -22,36 +22,50 @@ const getCodes = () => {
   });
 };
 
+const run = (input, a, b) => {
+  const codes = [...input];
+  codes[1] = a;
+  codes[2] = b;
+  let i = 0;
+  while (true) {
+    const opcode = codes[i];
+    const oneVal = codes[codes[i + 1]];
+    const twoVal = codes[codes[i + 2]];
+    const dest = codes[i + 3];
+    switch (opcode) {
+      case 1:
+        codes[dest] = oneVal + twoVal;
+        break;
+      case 2:
+        codes[dest] = oneVal * twoVal;
+        break;
+      case 99:
+        return codes;
+      default:
+        throw new Error(`Unknown opcode: ${opcode}`);
+    }
+    i += 4;
+  }
+};
+
 getCodes()
   .then(codes => {
     const copy = [...codes];
-    copy[1] = 12;
-    copy[2] = 2;
-    return copy;
-  })
-  .then(c => {
-    const codes = [...c];
-    let i = 0;
-    while (true) {
-      const opcode = codes[i];
-      const oneVal = codes[codes[i + 1]];
-      const twoVal = codes[codes[i + 2]];
-      const dest = codes[i + 3];
-      switch (opcode) {
-        case 1:
-          codes[dest] = oneVal + twoVal;
-          break;
-        case 2:
-          codes[dest] = oneVal * twoVal;
-          break;
-        case 99:
-          return codes;
-        default:
-          throw new Error(`Unknown opcode: ${opcode}`);
+    for (let i = 0; i < copy.length; i += 1) {
+      for (let j = 0; j < copy.length; j += 1) {
+        const [output] = run(copy, i, j);
+        if (output === 19690720) {
+          return [i, j];
+        }
       }
-      i += 4;
     }
+    throw new Error("No solution");
   })
-  .then(out => {
-    console.log(out);
+  .then(([noun, verb]) => {
+    console.log(`noun=${noun} verb=${verb}\t${noun}${verb}`);
+    process.exit(0);
+  })
+  .catch(ex => {
+    console.error(ex);
+    process.exit(1);
   });
