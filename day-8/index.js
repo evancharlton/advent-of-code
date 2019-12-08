437a117c10fa57d978eda99fd0011b0c7c1f6626
Evan Charlton <evancharlton@gmail.com>
2019-12-08 11:13:22 +0100

    Day 8 (part 2)

--- day-8/index.js
+++ day-8/index.js
@@ -17,22 +17,44 @@ const execute = (width, height) => {
       return layers;
     })
     .then(layers => {
-      return layers.map(layer => {
-        return layer.reduce((acc, pixel) => {
-          return {
-            ...acc,
-            [pixel]: (acc[pixel] || 0) + 1
-          };
-        }, {});
+      const combined = [];
+      for (let y = 0; y < height; y += 1) {
+        const row = [];
+        for (let x = 0; x < width; x += 1) {
+          row.push(2);
+        }
+        combined[y] = row;
+      }
+      layers.forEach(layer => {
+        for (let i = 0; i < layer.length; i += 1) {
+          const y = Math.floor(i / width);
+          const x = i % width;
+          const existingValue = combined[y][x];
+          if (existingValue === 2) {
+            combined[y][x] = layer[i];
+          }
+        }
       });
+      return combined;
     })
-    .then(layerPixelCounts => {
-      return layerPixelCounts.sort((a, b) => {
-        return a[0] - b[0];
-      })[0];
-    })
-    .then(leastZeroes => {
-      return leastZeroes[1] * leastZeroes[2];
+    .then(image => {
+      return image
+        .reduce((acc, row) => {
+          return [
+            ...acc,
+            row
+              .map(v => {
+                switch (+v) {
+                  case 0:
+                    return "#";
+                  case 1:
+                    return " ";
+                }
+              })
+              .join(" ")
+          ];
+        }, [])
+        .join("\n");
     });
 };
 
