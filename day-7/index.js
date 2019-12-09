05068a50f66477621d1f97772edcbbe68cece518
Evan Charlton <evancharlton@gmail.com>
2019-12-09 19:07:42 +0100

    Day 7 (part 2)

--- day-7/index.js
+++ day-7/index.js
@@ -1,62 +1,63 @@
 const readLines = require("../read-input");
 const intcode = require("../intcode");
 
-const TEST = false;
+const TEST =
+  false &&
+  "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
 
-const createRunner = program => {
-  return (phase, previousOutput) => {
-    let inputCount = 0;
-    let output = 0;
-    intcode(
-      program,
-      () => {
-        if (inputCount++ === 0) {
-          return phase;
-        }
-        return previousOutput;
-      },
-      out => {
-        output = out;
-      }
-    );
-    return output;
+const executeSequence = async (program, [a, b, c, d, e]) => {
+  let outputs = {
+    a: undefined,
+    b: undefined,
+    c: undefined,
+    d: undefined,
+    e: 0
   };
-};
 
-const executeSequence = (runner, [A, B, C, D, E]) => {
-  const outputA = runner(A, 0);
-  const outputB = runner(B, outputA);
-  const outputC = runner(C, outputB);
-  const outputD = runner(D, outputC);
-  const outputE = runner(E, outputD);
-  const log = (letter, phase, output) => `${letter}(${phase}, ${output})`;
-  // console.log(
-  //   [
-  //     log("A", A, 0),
-  //     log("B", B, outputA),
-  //     log("C", C, outputB),
-  //     log("D", D, outputC),
-  //     log("E", E, outputD)
-  //   ].join(" => "),
-  //   "=>",
-  //   outputE
-  // );
-  return outputE;
+  const getInput = (amp, phaseSetting) => {
+    let callCount = 0;
+    return () => {
+      if (callCount++ === 0) {
+        return phaseSetting;
+      }
+      return new Promise(resolve => {
+        (function getValue() {
+          if (outputs[amp] !== undefined) {
+            const output = outputs[amp];
+            outputs[amp] = undefined;
+            return resolve(output);
+          }
+          setTimeout(getValue, 1);
+        })();
+      });
+    };
+  };
+
+  const setOutput = amp => output => {
+    outputs[amp] = output;
+  };
+
+  const ampA = intcode(program, getInput("e", a), setOutput("a"));
+  const ampB = intcode(program, getInput("a", b), setOutput("b"));
+  const ampC = intcode(program, getInput("b", c), setOutput("c"));
+  const ampD = intcode(program, getInput("c", d), setOutput("d"));
+  const ampE = intcode(program, getInput("d", e), setOutput("e"));
+
+  const memories = await Promise.all([ampA, ampB, ampC, ampD, ampE]);
+
+  return outputs.e;
 };
 
 readLines("./day-7/input", TEST)
-  .then(([program]) => {
-    const runner = createRunner(program);
-    return runner;
-  })
-  .then(runner => {
+  .then(async ([program]) => {
     let maxOutput = 0;
     let sequence = "";
-    for (let a = 0; a <= 4; a += 1) {
-      for (let b = 0; b <= 4; b += 1) {
-        for (let c = 0; c <= 4; c += 1) {
-          for (let d = 0; d <= 4; d += 1) {
-            for (let e = 0; e <= 4; e += 1) {
+    const [min, max] = [5, 9];
+    for (let a = min; a <= max; a += 1) {
+      for (let b = min; b <= max; b += 1) {
+        for (let c = min; c <= max; c += 1) {
+          for (let d = min; d <= max; d += 1) {
+            for (let e = min; e <= max; e += 1) {
               if (
                 a === b ||
                 a === c ||
@@ -72,8 +73,7 @@ readLines("./day-7/input", TEST)
                 continue;
               }
               const input = [a, b, c, d, e];
-              const output = executeSequence(runner, input);
-              // console.log(input.join(","), "=>", output);
+              const output = await executeSequence(program, input);
               if (output > maxOutput) {
                 maxOutput = output;
                 sequence = input.join(",");
--- intcode/index.js
+++ intcode/index.js
@@ -36,58 +36,58 @@ const processInstruction = ins => {
   };
 };
 
-const getValue = (codes, parameter, mode) => {
+const getValue = (memory, parameter, mode) => {
   if (+mode === 1) {
     // Immediate mode
     return +parameter;
   } else {
     // Position mode
-    return +codes[parameter];
+    return +memory[parameter];
   }
 };
 
-const execute = (program, getInput, onOutput) => {
-  const codes = program.split(",").map(v => +v);
+const execute = async (program, getInput, onOutput) => {
+  const memory = program.split(",").map(v => +v);
   let i = 0;
   while (true) {
-    if (i >= codes.length) {
-      throw new Error(`i (${i}) >= codes.length (${codes.length})`);
+    if (i >= memory.length) {
+      throw new Error(`i (${i}) >= memory.length (${memory.length})`);
     }
-    const instruction = codes[i++];
+    const instruction = memory[i++];
     const { mode3, mode2, mode1, opcode } = processInstruction(instruction);
     switch (+opcode) {
       // Add
       case 1: {
-        const oneVal = getValue(codes, codes[i++], mode1);
-        const twoVal = getValue(codes, codes[i++], mode2);
+        const oneVal = getValue(memory, memory[i++], mode1);
+        const twoVal = getValue(memory, memory[i++], mode2);
         const result = oneVal + twoVal;
-        codes[codes[i++]] = result;
+        memory[memory[i++]] = result;
         break;
       }
       // Multiply
       case 2: {
-        const oneVal = getValue(codes, codes[i++], mode1);
-        const twoVal = getValue(codes, codes[i++], mode2);
+        const oneVal = getValue(memory, memory[i++], mode1);
+        const twoVal = getValue(memory, memory[i++], mode2);
         const result = oneVal * twoVal;
-        codes[codes[i++]] = result;
+        memory[memory[i++]] = result;
         break;
       }
       // Input
       case 3: {
-        const inputLocation = codes[i++];
-        codes[inputLocation] = getInput();
+        const inputLocation = memory[i++];
+        memory[inputLocation] = await getInput();
         break;
       }
       // Output
       case 4: {
-        const output = getValue(codes, codes[i++], mode1);
+        const output = getValue(memory, memory[i++], mode1);
         onOutput(output);
         break;
       }
       // jump-if-true
       case 5: {
-        const firstVal = getValue(codes, codes[i++], mode1);
-        const jumpTo = getValue(codes, codes[i++], mode2);
+        const firstVal = getValue(memory, memory[i++], mode1);
+        const jumpTo = getValue(memory, memory[i++], mode2);
         if (firstVal !== 0) {
           i = jumpTo;
         }
@@ -95,8 +95,8 @@ const execute = (program, getInput, onOutput) => {
       }
       // jump-if-false
       case 6: {
-        const firstVal = getValue(codes, codes[i++], mode1);
-        const jumpTo = getValue(codes, codes[i++], mode2);
+        const firstVal = getValue(memory, memory[i++], mode1);
+        const jumpTo = getValue(memory, memory[i++], mode2);
         if (firstVal === 0) {
           i = jumpTo;
         }
@@ -104,26 +104,26 @@ const execute = (program, getInput, onOutput) => {
       }
       // less than
       case 7: {
-        const firstVal = getValue(codes, codes[i++], mode1);
-        const secondVal = getValue(codes, codes[i++], mode2);
-        const thirdVal = getValue(codes, codes[i++], 1);
-        codes[thirdVal] = firstVal < secondVal ? 1 : 0;
+        const firstVal = getValue(memory, memory[i++], mode1);
+        const secondVal = getValue(memory, memory[i++], mode2);
+        const thirdVal = getValue(memory, memory[i++], 1);
+        memory[thirdVal] = firstVal < secondVal ? 1 : 0;
         break;
       }
       // equals
       case 8: {
-        const firstVal = getValue(codes, codes[i++], mode1);
-        const secondVal = getValue(codes, codes[i++], mode2);
-        const thirdVal = getValue(codes, codes[i++], 1);
-        codes[thirdVal] = firstVal === secondVal ? 1 : 0;
+        const firstVal = getValue(memory, memory[i++], mode1);
+        const secondVal = getValue(memory, memory[i++], mode2);
+        const thirdVal = getValue(memory, memory[i++], 1);
+        memory[thirdVal] = firstVal === secondVal ? 1 : 0;
         break;
       }
       // Terminate
       case 99: {
-        return;
+        return memory;
       }
       default: {
-        throw new Error(`Unknown opcode: ${opcode} (${i}, ${codes[i]})`);
+        throw new Error(`Unknown opcode: ${opcode} (${i}, ${memory[i]})`);
       }
     }
   }
