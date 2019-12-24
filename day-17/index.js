ab417227b572696c9086b1f305fc904d82efd647
Evan Charlton <evancharlton@gmail.com>
2019-12-24 11:29:48 +0100

    Day 17 (part 2)

--- day-17/index.js
+++ day-17/index.js
@@ -3,54 +3,51 @@ const intcode = require("../intcode");
 
 const TEST = "";
 
-const INTERSECTIONS = [401, 612, 500].reduce(
-  (acc, n) => ({ ...acc, [n]: true }),
-  {}
-);
+const INPUTS = [
+  // main input
+  "A,B,B,C,B,C,B,C,A,A",
+  // A program
+  "L,6,R,8,L,4,R,8,L,12",
+  // B program
+  "L,12,R,10,L,4",
+  // C program
+  "L,12,L,6,L,4,L,4",
+  // Video feed?
+  "y"
+].reduce((acc, routine, i) => {
+  return [
+    ...acc,
+    ...routine.split("").map(v => String(v).charCodeAt(0)),
+    "\n".charCodeAt(0)
+  ];
+}, []);
 
 readLines("./day-17/input", TEST)
   .then(async ([program]) => {
+    return `2${program.substr(1)}`;
+  })
+  .then(async program => {
     const outputs = [];
     await intcode(
       program,
       () => {
-        return 0;
+        const input = INPUTS.shift();
+        // console.log(
+        //   `Sending ${input} (${
+        //     input === 10 ? "\\n" : String.fromCharCode(input)
+        //   })`
+        // );
+        return input;
       },
       out => {
-        outputs.push(out);
+        if (out > 200) {
+          console.log(out);
+        } else {
+          outputs.push(out);
+        }
       }
     );
     return outputs.map(v => String.fromCharCode(+v));
-  })
-  .then(chars => {
-    const width =
-      chars.findIndex(v => {
-        return v === "\n";
-      }) + 1;
-    const XYs = {};
-    const glyphs = chars.map((v, i) => {
-      if (
-        chars[i] === "#" &&
-        chars[i - 1] === "#" &&
-        chars[i + 1] === "#" &&
-        chars[i - width] === "#" &&
-        chars[i + width] === "#"
-      ) {
-        const x = i % width;
-        const y = Math.floor(i / width);
-        XYs[`${x},${y}`] = x * y;
-        return "O";
-      }
-      return v;
-    });
-    return { glyphs, XYs };
-  })
-  .then(({ glyphs, XYs }) => {
-    const sum = Object.keys(XYs).reduce((acc, xy) => {
-      return acc + XYs[xy];
-    }, 0);
-    console.log("Sum:", sum);
-    return glyphs;
   })
   .then(chars => {
     return ` ${chars.join(" ")}`;
