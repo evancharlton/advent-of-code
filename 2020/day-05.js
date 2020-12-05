1b0df6c361965aabc3fcb86ec6a39f8c9b9402bb
Evan Charlton <evancharlton@gmail.com>
2020-12-05 07:51:12 +0100

    Day 5 (part 1)

--- 2020/day-05.js
+++ 2020/day-05.js
@@ -1,1 +1,45 @@
-const lines = require("./input")(__filename);
+const lines = require("./input")(__filename, "\n");
+
+const getSeatInfo = (input) => {
+  const letters = input.split("");
+  const rowInfo = letters.slice(0, 7);
+  const seatInfo = letters.slice(7);
+
+  const row = rowInfo.reduce(
+    ([start, end], l) => {
+      if (end - start === 1) {
+        return l === "F" ? start : end;
+      }
+      const mid = start + (end - start) / 2;
+      if (l === "F") {
+        return [start, Math.floor(mid)];
+      } else {
+        return [Math.ceil(mid), end];
+      }
+    },
+    [0, 127]
+  );
+
+  const seat = seatInfo.reduce(
+    ([start, end], l) => {
+      if (end - start === 1) {
+        return l === "L" ? start : end;
+      }
+      const mid = start + (end - start) / 2;
+      if (l === "L") {
+        return [start, Math.floor(mid)];
+      } else {
+        return [Math.ceil(mid), end];
+      }
+    },
+    [0, 7]
+  );
+
+  return { row, seat, id: row * 8 + seat };
+};
+
+const ids = lines.map(getSeatInfo).sort(({ id: idA }, { id: idB }) => {
+  return idB - idA;
+});
+
+console.log(ids[0]);
