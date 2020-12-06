c333096ff888d1c9e4197ccfb99f162768295799
Evan Charlton <evancharlton@gmail.com>
2020-12-06 06:53:25 +0100

    Day 6 (part 2)

--- 2020/day-06.js
+++ 2020/day-06.js
@@ -1,8 +1,14 @@
 const groups = require("./input")(__filename, "\n\n", process.argv[2]);
 
 const uniques = groups.map((group) => {
-  const answers = group.replace(/\n/g, "").split("");
-  return new Set(answers).size;
+  const responses = group.split("\n").map((line) => line.split(""));
+  const out = responses.reduce((acc, person) => {
+    if (acc === undefined) {
+      return person;
+    }
+    return acc.filter((v) => person.includes(v));
+  }, undefined);
+  return out.length;
 });
 
 console.log(uniques.reduce((acc, n) => acc + n, 0));
