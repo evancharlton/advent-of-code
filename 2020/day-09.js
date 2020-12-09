8e8a48bfb93af89c3cbfe9a93cff8ab573d98cae
Evan Charlton <evancharlton@gmail.com>
2020-12-09 06:38:08 +0100

    Day 9 (part 1)

--- 2020/day-09.js
+++ 2020/day-09.js
@@ -1,1 +1,28 @@
-const lines = require("./input")(__filename);
+const lines = require("./input")(__filename).map(Number);
+
+const getValidNumbers = (preambleArr) => {
+  const additions = new Set();
+  for (let i = 0; i < preambleArr.length; i += 1) {
+    for (let j = 0; j < preambleArr.length; j += 1) {
+      if (i === j) {
+        continue;
+      }
+
+      additions.add(preambleArr[i] + preambleArr[j]);
+    }
+  }
+
+  return additions;
+};
+
+const PREAMBLE_SIZE = 25;
+
+for (let i = PREAMBLE_SIZE; i < lines.length; i += 1) {
+  const value = lines[i];
+  const preamble = lines.slice(i - PREAMBLE_SIZE, i);
+  const validNumbers = getValidNumbers(preamble);
+  if (!validNumbers.has(value)) {
+    console.error(`${value} is not valid`);
+    break;
+  }
+}
