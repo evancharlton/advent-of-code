cee5efa1012e8cd4401eb8327ab97d85396ab2a0
Evan Charlton <evancharlton@gmail.com>
2020-12-10 08:16:05 +0100

    Day 10 (part 2)
    I'm not happy about this solution, but it works. But seriously: yikes.

--- 2020/day-10.js
+++ 2020/day-10.js
@@ -1,24 +1,69 @@
 const lines = require("./input")(__filename).map(Number);
 
+lines.push(0);
 lines.sort((a, b) => a - b);
 
-let joltage = 0;
-const spreads = {};
-while (lines.length > 0) {
-  const next = lines.shift();
-  const difference = next - joltage;
-  if (difference > 3 || difference < 1) {
-    console.warn(`${next} is incompatible with ${joltage}`);
-    break;
-  }
+const runs = [];
+let run = [];
+for (let i = 0; i < lines.length; i += 1) {
+  const curr = lines[i];
+  const next = lines[i + 1];
+  const breakpoint = next - curr === 3;
+  run.push(curr);
 
-  joltage = next;
-  spreads[difference] = (spreads[difference] || 0) + 1;
+  if (breakpoint) {
+    // This is the end of a run; save it away.
+    runs.push([...run]);
+    run = [];
+  }
 }
 
-// Account for the device itself
-joltage += 3;
-spreads[3] = spreads[3] + 1;
+if (run.length > 0) {
+  runs.push([...run]);
+}
 
-console.table(spreads);
-console.log(spreads[1] * spreads[3]);
+const isValid = (list) => {
+  for (let i = 1; i < list.length; i += 1) {
+    if (list[i] - list[i - 1] > 3) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Yanked from SO
+const powerset = (l) => {
+  return (function ps(list) {
+    if (list.length === 0) {
+      return [[]];
+    }
+    var head = list.pop();
+    var tailPS = ps(list);
+    return tailPS.concat(
+      tailPS.map(function (e) {
+        return [head].concat(e);
+      })
+    );
+  })(l.slice()).sort((a, b) => a - b);
+};
+
+const bruteforce = (list) => {
+  const innerSet = list.slice(1, list.length - 1);
+  return powerset(innerSet).reduce((acc, set) => {
+    const paddedSet = [list[0], ...set, list[list.length - 1]].sort(
+      (a, b) => a - b
+    );
+    const valid = isValid(paddedSet);
+    if (valid) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+};
+
+const relevant = runs.filter((r) => r.length > 2);
+const out = relevant.reduce((acc, r) => {
+  return acc * bruteforce(r);
+}, 1);
+
+console.log("total:", out);
