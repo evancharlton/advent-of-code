eb86b663aeab3866ed767b0f6711eb286ab8393d
Evan Charlton <evancharlton@gmail.com>
2020-12-11 07:48:10 +0100

    Day 11 (part 2)

--- 2020/day-11.js
+++ 2020/day-11.js
@@ -3,34 +3,122 @@ const data = (type = "") => {
   return lines;
 };
 
-const tick = (plane) => {
+const immediateNeighbors = (plane, r, c) => {
+  const rowAbove = plane[r - 1] || [];
+  const row = plane[r];
+  const rowBelow = plane[r + 1] || [];
+
+  const w = row[c - 1] || "L";
+  const nw = rowAbove[c - 1] || "L";
+  const n = rowAbove[c] || "L";
+  const ne = rowAbove[c + 1] || "L";
+  const e = row[c + 1] || "L";
+  const se = rowBelow[c + 1] || "L";
+  const s = rowBelow[c] || "L";
+  const sw = rowBelow[c - 1] || "L";
+
+  return [w, nw, n, ne, e, se, s, sw].reduce((acc, state) => {
+    if (state === "#") {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+};
+
+const visibleNeighbors = (plane, r, c) => {
+  const row = plane[r];
+
+  let n = ".";
+  let ne = ".";
+  let e = ".";
+  let se = ".";
+  let s = ".";
+  let sw = ".";
+  let w = ".";
+  let nw = ".";
+
+  let y = r;
+  let x = c;
+
+  // walk north
+  y = r - 1;
+  while (y >= 0 && n === ".") {
+    n = (plane[y] || [])[c] || ".";
+    y -= 1;
+  }
+
+  // walk south
+  y = r + 1;
+  while (y <= plane.length && s === ".") {
+    s = (plane[y] || [])[c] || ".";
+    y += 1;
+  }
+
+  // walk east
+  x = c + 1;
+  while (x < row.length && e === ".") {
+    e = row[x] || ".";
+    x += 1;
+  }
+
+  // walk west
+  x = c - 1;
+  while (x >= 0 && w === ".") {
+    w = row[x] || ".";
+    x -= 1;
+  }
+
+  // walk northeast
+  x = c + 1;
+  y = r - 1;
+  while (x < row.length && y >= 0 && ne === ".") {
+    ne = (plane[y] || [])[x] || ".";
+    y -= 1;
+    x += 1;
+  }
+
+  // walk southeast
+  x = c + 1;
+  y = r + 1;
+  while (x < row.length && y <= plane.length && se === ".") {
+    se = (plane[y] || [])[x] || ".";
+    y += 1;
+    x += 1;
+  }
+
+  // walk southwest
+  x = c - 1;
+  y = r + 1;
+  while (x >= 0 && y <= plane.length && sw === ".") {
+    sw = (plane[y] || [])[x] || ".";
+    y += 1;
+    x -= 1;
+  }
+
+  // walk northwest
+  x = c - 1;
+  y = r - 1;
+  while (x >= 0 && y >= 0 && nw === ".") {
+    nw = (plane[y] || [])[x] || ".";
+    y -= 1;
+    x -= 1;
+  }
+
+  const arr = { w, nw, n, ne, e, se, s, sw };
+  return Object.values(arr).filter((v) => v === "#").length;
+};
+
+const tick = (alg, tolerance) => (plane) => {
   const next = [];
   let changes = 0;
 
   for (let r = 0; r < plane.length; r += 1) {
     next[r] = [];
 
-    const rowAbove = plane[r - 1] || [];
     const row = plane[r];
-    const rowBelow = plane[r + 1] || [];
     for (let c = 0; c < row.length; c += 1) {
       const seat = row[c];
-
-      const w = row[c - 1] || "L";
-      const nw = rowAbove[c - 1] || "L";
-      const n = rowAbove[c] || "L";
-      const ne = rowAbove[c + 1] || "L";
-      const e = row[c + 1] || "L";
-      const se = rowBelow[c + 1] || "L";
-      const s = rowBelow[c] || "L";
-      const sw = rowBelow[c - 1] || "L";
-
-      const neighbors = [w, nw, n, ne, e, se, s, sw].reduce((acc, state) => {
-        if (state === "#") {
-          return acc + 1;
-        }
-        return acc;
-      }, 0);
+      const neighbors = alg(plane, r, c);
 
       let state = seat;
       if (seat === ".") {
@@ -42,7 +130,7 @@ const tick = (plane) => {
         }
       } else if (seat === "#") {
         // Seat is occupied
-        if (neighbors >= 4) {
+        if (neighbors >= tolerance) {
           // Corona mode activated
           state = "L";
         }
@@ -85,22 +173,38 @@ const part1 = (lines) => {
   const initial = fillPlane(lines);
   let changes = 1;
   let state = initial;
+  let loops = 0;
   while (changes !== 0) {
-    const { next, changes: newChanges } = tick(state);
+    if (loops++ >= 10000) {
+      throw new Error("Infinite loop");
+    }
+    const { next, changes: newChanges } = tick(immediateNeighbors, 4)(state);
     changes = newChanges;
     state = next;
-    // console.log(state.map((l) => l.join("")).join("\n"));
   }
 
   return occupied(state);
 };
 
 const part2 = (lines) => {
-  return undefined;
+  const initial = fillPlane(lines);
+  let changes = 1;
+  let state = initial;
+  let loops = 0;
+  while (changes !== 0) {
+    if (loops++ >= 10000) {
+      throw new Error("Infinite loop");
+    }
+    const { next, changes: newChanges } = tick(visibleNeighbors, 5)(state);
+    changes = newChanges;
+    state = next;
+  }
+
+  return occupied(state);
 };
 
 if (!process.argv.includes("--watch")) {
-  console.log(part1(data(process.argv[2] || "")));
+  console.log(part2(data(process.argv[2] || "")));
 }
 
 module.exports = {
@@ -109,4 +213,6 @@ module.exports = {
   part1,
   part2,
   toString,
+  immediateNeighbors,
+  visibleNeighbors,
 };
--- 2020/day-11.spec.js
+++ 2020/day-11.spec.js
@@ -1,12 +1,65 @@
-const { part1, part2, data, tick, toString } = require("./day-11");
+const {
+  part1,
+  part2,
+  data,
+  tick,
+  toString,
+  immediateNeighbors,
+  visibleNeighbors,
+} = require("./day-11");
 
 describe("Day 11", () => {
   describe("tick", () => {
     it("Detects final state", () => {
       const start = data("test-final");
-      const { next, changes } = tick(start);
+      const { next, changes } = tick(immediateNeighbors, 4)(start);
       expect(toString(start)).toMatch(toString(next));
       expect(changes).toBe(0);
+    });
+  });
+
+  describe("visibleNeighbors", () => {
+    it("handles the row", () => {
+      const input = `
+        .............
+        .L.L.#.#.#.#.
+        .............
+      `
+        .trim()
+        .split("\n")
+        .map((l) => l.trim());
+      expect(visibleNeighbors(input, 1, 1)).toBe(0);
+      expect(visibleNeighbors(input, 1, 3)).toBe(1);
+      expect(visibleNeighbors(input, 1, 5)).toBe(1);
+      expect(visibleNeighbors(input, 1, 7)).toBe(2);
+    });
+
+    it("handles the zero-case", () => {
+      const input = `
+        .##.##.
+        #.#.#.#
+        ##...##
+        ...L...
+        ##...##
+        #.#.#.#
+        .##.##.
+      `
+        .trim()
+        .split("\n")
+        .map((l) => l.trim());
+      expect(visibleNeighbors(input, 3, 3)).toBe(0);
+    });
+
+    it("handles the eight-case", () => {
+      const input = `
+        ###
+        #L#
+        ###
+      `
+        .trim()
+        .split("\n")
+        .map((l) => l.trim());
+      expect(visibleNeighbors(input, 1, 1)).toBe(8);
     });
   });
 
@@ -18,7 +71,7 @@ describe("Day 11", () => {
 
   describe("Part 2", () => {
     it("works for test data", () => {
-      expect(part2(data("test"))).toBe(undefined);
+      expect(part2(data("test"))).toBe(26);
     });
   });
 });
