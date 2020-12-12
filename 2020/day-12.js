a6c5dbd13bc737bbb732e93b656cb7c9be7302ba
Evan Charlton <evancharltoN@gmail.com>
2020-12-12 07:00:45 +0100

    Day 12 (part 2)

--- 2020/day-12.js
+++ 2020/day-12.js
@@ -96,8 +96,68 @@ const part1 = (actions) => {
   return Math.abs(ship.x) + Math.abs(ship.y);
 };
 
-const part2 = (lines) => {
-  undefined;
+const MUTATORS = {
+  [0]: (w) => w,
+  [90]: ({ x, y }) => ({ x: y, y: -x }),
+  [180]: ({ x, y }) => ({ x: -x, y: -y }),
+  [270]: ({ x, y }) => ({ x: -y, y: x }),
+};
+
+const rotate = ({ action, value }, { x, y }) => {
+  switch (action) {
+    case "R": {
+      return MUTATORS[value]({ x, y });
+    }
+
+    case "L":
+      return rotate({ action: "R", value: (-value + 360) % 360 }, { x, y });
+
+    default:
+      throw new Error(`Unknown rotation: ${action}`);
+  }
+};
+
+const part2 = (actions) => {
+  const waypoint = {
+    x: 10,
+    y: 1,
+  };
+
+  const ship = {
+    x: 0,
+    y: 0,
+    d: "E",
+  };
+
+  actions.forEach(({ action, value }) => {
+    switch (action) {
+      case "N":
+      case "E":
+      case "S":
+      case "W": {
+        const { x, y } = advance({ action, value });
+        waypoint.x += x;
+        waypoint.y += y;
+        break;
+      }
+
+      case "F": {
+        ship.x += waypoint.x * value;
+        ship.y += waypoint.y * value;
+        break;
+      }
+
+      case "R":
+      case "L": {
+        const { x, y } = rotate({ action, value }, waypoint);
+        waypoint.x = x;
+        waypoint.y = y;
+        break;
+      }
+    }
+  });
+
+  return Math.abs(ship.x) + Math.abs(ship.y);
 };
 
 if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
--- 2020/day-12.spec.js
+++ 2020/day-12.spec.js
@@ -5,11 +5,19 @@ describe("Day 12", () => {
     it("works for test data", () => {
       expect(part1(data("test"))).toBe(25);
     });
+
+    it("works for real data", () => {
+      expect(part1(data())).toBe(2879);
+    });
   });
 
   describe("Part 2", () => {
     it("works for test data", () => {
-      expect(part2(data("test"))).toBe(undefined);
+      expect(part2(data("test"))).toBe(286);
+    });
+
+    it("works for real data", () => {
+      expect(part2(data())).toBe(178986);
     });
   });
 });
