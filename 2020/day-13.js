a656172fb91eacf07da6518c86475ab02eecad74
Evan Charlton <evancharltoN@gmail.com>
2020-12-12 07:03:23 +0100

    chore: Got set up for day 13
    If I do one more day of this unchanged pattern, then I'll do it for all
    the remaining days.

--- 2020/day-13.js
+++ 2020/day-13.js
@@ -1,1 +1,23 @@
-const lines = require("./input")(__filename);
+const data = (type = "") => {
+  const lines = require("./input")(__filename, "\n", type);
+  return lines;
+};
+
+const part1 = (lines) => {
+  // TODO
+};
+
+const part2 = (lines) => {
+  // TODO
+};
+
+if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
+  console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+  console.log(`Part 2:`, part2(data(process.argv[2] || "")));
+}
+
+module.exports = {
+  data,
+  part1,
+  part2,
+};
