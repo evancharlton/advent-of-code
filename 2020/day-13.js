d61b5a6145e89f0a87e7fba7fd7219bd65e69e21
Evan Charlton <evancharltoN@gmail.com>
2020-12-14 08:47:10 +0100

    Day 13 (part 2)
    I finally have something written that I'm reasonably happy with.

--- 2020/day-13.js
+++ 2020/day-13.js
@@ -1,34 +1,66 @@
 const data = (type = "") => {
   const lines = require("./input")(__filename, "\n", type);
   const [earliest, schedules] = lines;
+  return { earliest: +earliest, schedules };
+};
+
+const part1 = ({ earliest, schedules }) => {
   const buses = schedules
     .split(",")
     .filter((s) => s !== "x")
     .map(Number);
-  return { earliest: +earliest, buses };
-};
 
-const part1 = ({ earliest, buses }) => {
   let now = earliest;
   while (true) {
-    const leaving = buses.find((id) => {
+    const departingBusId = buses.find((id) => {
       return now % id === 0;
     });
-    if (leaving) {
-      return leaving * (now - earliest);
+    if (departingBusId) {
+      return departingBusId * (now - earliest);
     }
     now += 1;
   }
 };
 
-const part2 = (lines) => {
-  // TODO
+const part2 = ({ schedules }) => {
+  const { time } = schedules
+    .split(",")
+    .map(Number)
+    .reduce(
+      (acc, busID, i) => {
+        if (i === 0) {
+          return { step: busID, time: busID };
+        }
+
+        if (Number.isNaN(busID)) {
+          return acc;
+        }
+
+        let { step, time } = acc;
+        while ((time + i) % busID !== 0) {
+          time += step;
+        }
+
+        return { step: step * busID, time };
+      },
+      { step: 0, time: 0 }
+    );
+
+  return time;
 };
 
 if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
   console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+
+  console.log(`Part 2:`, part2({ schedules: "17,x,13,19" }));
+  console.log(`Part 2:`, part2({ schedules: "67,7,59,61" }));
+  console.log(`Part 2:`, part2({ schedules: "67,x,7,59,61" }));
+  console.log(`Part 2:`, part2({ schedules: "67,7,x,59,61" }));
+  console.log(`Part 2:`, part2({ schedules: "1789,37,47,1889" }));
   console.log(`Part 2:`, part2(data(process.argv[2] || "")));
 }
+
+// 440 915 240 570 129 088
 
 module.exports = {
   data,
--- 2020/day-13.spec.js
+++ 2020/day-13.spec.js
@@ -12,12 +12,18 @@ describe("Day 13", () => {
   });
 
   describe("Part 2", () => {
+    const simplePart2 = (schedules) => part2({ schedules });
+
     it("works for test data", () => {
-      expect(part2(data("test"))).toBe(undefined);
+      expect(simplePart2("17,x,13,19")).toBe(3417);
+      expect(simplePart2("67,7,59,61")).toBe(754018);
+      expect(simplePart2("67,x,7,59,61")).toBe(779210);
+      expect(simplePart2("67,7,x,59,61")).toBe(1261476);
+      expect(simplePart2("1789,37,47,1889")).toBe(1202161486);
     });
 
-    it.skip("works for real data", () => {
-      expect(part2(data())).toBe(undefined);
+    it("works for real data", () => {
+      expect(part2(data())).toBe(230903629977901);
     });
   });
 });
