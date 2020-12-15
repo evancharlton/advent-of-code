7f10bda7e494ab1dfd958e49d8f44f9ca19b2ef2
Evan Charlton <evancharlton@gmail.com>
2020-12-15 06:49:44 +0100

    Day 15 (part 1)

--- 2020/day-15.js
+++ 2020/day-15.js
@@ -1,13 +1,42 @@
 const data = (type = "") => {
-  const lines = require("./input")(__filename, "\n", type);
-  return lines;
+  return require("./input")(__filename, ",", type).map(Number);
 };
 
-const part1 = (lines) => {
-  return undefined;
+const part1 = (numbers, limit = 2020) => {
+  const spoken = {};
+
+  let said;
+  let i = 1;
+  numbers.forEach((n, j) => {
+    spoken[n] = [i];
+    said = n;
+    i += 1;
+  });
+
+  while (i <= limit) {
+    let [turn, prev] = spoken[said];
+
+    // Do we have a record of this?
+    if (turn === undefined) {
+      throw new Error(`${said} was spoken for the first time or something`);
+    }
+
+    // That was the first time it has been spoken
+    if (prev === undefined) {
+      prev = turn;
+    }
+
+    said = turn - prev;
+    [turn] = spoken[said] || [i];
+
+    spoken[said] = [i, turn];
+    i += 1;
+  }
+
+  return said;
 };
 
-const part2 = (lines) => {
+const part2 = (numbers) => {
   return undefined;
 };
 
--- 2020/day-15.spec.js
+++ 2020/day-15.spec.js
@@ -3,7 +3,17 @@ const { part1, part2, data } = require("./day-15");
 describe("Day 15", () => {
   describe("Part 1", () => {
     it("works for test data", () => {
-      expect(part1(data("test"))).toBe(undefined);
+      // expect(part1([0], 1)).toBe(0);
+      // expect(part1([0, 3], 2)).toBe(3);
+      // expect(part1([0, 3, 6], 3)).toBe(6);
+      // expect(part1([0, 3, 6], 4)).toBe(0);
+      // expect(part1([0, 3, 6], 5)).toBe(3);
+      // expect(part1([0, 3, 6], 6)).toBe(3);
+      expect(part1([0, 3, 6], 7)).toBe(1);
+      expect(part1([0, 3, 6], 8)).toBe(0);
+      expect(part1([0, 3, 6], 9)).toBe(4);
+      expect(part1([0, 3, 6], 10)).toBe(0);
+      expect(part1([0, 3, 6])).toBe(436);
     });
 
     it.skip("works for real data", () => {
