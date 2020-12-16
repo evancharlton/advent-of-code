4a9ae199dad916df700532af22c609526dc533a1
Evan Charlton <evancharlton@gmail.com>
2020-12-16 05:57:35 +0100

    chore: Day 17 boilerplate

--- 2020/day-17.js
+++ 2020/day-17.js
@@ -1,1 +1,23 @@
-const lines = require("./input")(__filename);
+const data = (type = "") => {
+  const lines = require("./input")(__filename, "\n", type);
+  return lines;
+};
+
+const part1 = (lines) => {
+  return undefined;
+};
+
+const part2 = (lines) => {
+  return undefined;
+};
+
+if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
+  console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+  console.log(`Part 2:`, part2(data(process.argv[2] || "")));
+}
+
+module.exports = {
+  data,
+  part1,
+  part2,
+};
---
+++ 2020/day-17.spec.js
@@ -0,0 +1,23 @@
+const { part1, part2, data } = require("./day-16");
+
+describe("Day 17", () => {
+  describe("Part 1", () => {
+    it("works for test data", () => {
+      expect(part1(data("test"))).toBe(undefined);
+    });
+
+    it.skip("works for real data", () => {
+      expect(part1(data())).toBe(undefined);
+    });
+  });
+
+  describe("Part 2", () => {
+    it("works for test data", () => {
+      expect(part2(data("test"))).toBe(undefined);
+    });
+
+    it.skip("works for real data", () => {
+      expect(part2(data())).toBe(undefined);
+    });
+  });
+});
