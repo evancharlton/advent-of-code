0eeb74cb7c54739fb22c40838eea3b7347687a28
Evan Charlton <evancharlton@gmail.com>
2020-12-18 09:09:19 +0100

    chore: Allow year to be provided to get-input

--- 2020/get-input.js
+++ 2020/get-input.js
@@ -17,17 +17,20 @@ const date =
     return String(date);
   })();
 
+const year =
+  process.argv[3] ||
+  (() => {
+    const d = new Date();
+    return String(d.getFullYear());
+  })();
+
 const name = `input/day-${date}.txt`;
 const out = fs.createWriteStream(path.resolve(path.join(__dirname, name)));
 
 console.log(`Fetching input into ${name}`);
 
 http.get(
-  `https://adventofcode.com/2020/day/${date}/input`,
-  {
-    headers: {
-      cookie: `session=${process.env.AOC_COOKIE}`,
-    },
-  },
+  `https://adventofcode.com/${year}/day/${date}/input`,
+  { headers: { cookie: `session=${process.env.AOC_COOKIE}` } },
   (res) => res.pipe(out)
 );
