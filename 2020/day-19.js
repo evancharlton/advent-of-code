f89bec0c92befb57382834fbb3def1289c5cd49f
Evan Charlton <evancharlton@gmail.com>
2020-12-19 08:11:00 +0100

    Day 19 (part 1)

--- 2020/day-19.js
+++ 2020/day-19.js
@@ -1,10 +1,53 @@
 const data = (type = "") => {
-  const lines = require("./input")(__filename, "\n", type);
-  return lines;
+  const [rules, receivedMessages] = require("./input")(
+    __filename,
+    "\n\n",
+    type
+  );
+  return [rules, receivedMessages.split("\n")];
 };
 
-const part1 = (lines) => {
-  return undefined;
+const expandZeroRule = (block) => {
+  const lines = block.split("\n").map((line) => line.replace(/"/g, ""));
+
+  const rules = lines
+    .map((line) => line.split(": "))
+    .map(([id, spec]) => {
+      let term = spec;
+      if (spec.includes("|")) {
+        term = spec
+          .split(" | ")
+          .map((v) => `(?:${v})`)
+          .join("|");
+      }
+      return [id, `(?:${term})`];
+    })
+    .reduce((acc, [id, regex]) => ({ ...acc, [id]: regex }), {});
+
+  while (rules["0"].match(/[\d]+/)) {
+    Object.entries(rules)
+      .filter(([id, regex]) => !regex.match(/[\d]/))
+      .forEach(([id, regex]) => {
+        Object.entries(rules)
+          .filter(([otherId]) => id !== otherId)
+          .forEach(([otherId, otherRegex]) => {
+            const re = new RegExp(`\\b${id}\\b`, "g");
+            if (!otherRegex.match(re)) {
+              return;
+            }
+            rules[otherId] = otherRegex.replace(re, regex);
+          });
+      });
+  }
+
+  const zeroRule = rules["0"].replace(/ /g, "");
+  return `^${zeroRule}$`;
+};
+
+const part1 = ([rules, messages]) => {
+  const zeroRule = expandZeroRule(rules);
+  const re = new RegExp(zeroRule);
+  return messages.filter((message) => message.match(re)).length;
 };
 
 const part2 = (lines) => {
--- 2020/day-19.spec.js
+++ 2020/day-19.spec.js
@@ -3,7 +3,7 @@ const { part1, part2, data } = require("./day-19");
 describe("Day 19", () => {
   describe("Part 1", () => {
     it("works for test data", () => {
-      expect(part1(data("test"))).toBe(undefined);
+      expect(part1(data("test"))).toBe(2);
     });
 
     it.skip("works for real data", () => {
