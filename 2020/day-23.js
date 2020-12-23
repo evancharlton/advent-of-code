62c28aa477f92f1f0b37b284d5efb1099efb2ff6
Evan Charlton <evancharlton@gmail.com>
2020-12-23 07:41:00 +0100

    Day 23 (part 1)

--- 2020/day-23.js
+++ 2020/day-23.js
@@ -1,15 +1,79 @@
 const data = (type = "") => {
-  const lines = require("./input")(__filename, "\n", type);
+  const lines = require("./input")(__filename, "", type).map(Number);
   return lines;
 };
 
-const part1 = (lines) => {
+const getNextValue = (cups, current) => {
+  const [lowest, highest] = minmax(cups);
+
+  if (current === lowest) {
+    return highest;
+  }
+
+  let c = current - 1;
+  while (!cups.includes(c)) {
+    c -= 1;
+  }
+  return c;
+};
+
+const debug = (...args) => {
+  if (process.env.NODE_ENV === "test") return;
+  console.log(...args);
+};
+
+const part1 = (numbers, limit = 100) => {
+  let turn = 0;
+  let cups = [...numbers];
+  while (turn++ < limit) {
+    debug(`-- move ${turn} --`);
+    const [current, a, b, c, ...rest] = cups;
+    const [next] = rest;
+    const picked = [current, ...rest];
+    debug(
+      `cups: ${cups
+        .map((c, i) => {
+          if (i === 0) {
+            return `(${c})`;
+          }
+          return c;
+        })
+        .join(" ")}`
+    );
+    debug(`pickup: ${a}, ${b}, ${c}`);
+
+    const destination = getNextValue(picked, current);
+    const destinationIndex = picked.indexOf(destination);
+    debug(`destination: ${destination} @ ${destinationIndex}`);
+    picked.splice(destinationIndex + 1, 0, a, b, c);
+
+    const nextIndex = picked.indexOf(next);
+    const before = picked.slice(0, nextIndex);
+    const after = picked.slice(nextIndex);
+    const nextArray = [...after, ...before];
+    cups = nextArray;
+
+    debug("");
+  }
+
+  const out = [];
+  const oneIndex = cups.indexOf(1);
+  for (let i = 1; i <= cups.length - 1; i += 1) {
+    out.push(cups[(oneIndex + i) % cups.length]);
+  }
+
+  return out.join("");
+};
+
+const part2 = (input) => {
   return undefined;
 };
 
-const part2 = (lines) => {
-  return undefined;
-};
+const minmax = (cups) =>
+  cups.reduce(([min, max], c) => [Math.min(min, c), Math.max(max, c)], [
+    Number.MAX_SAFE_INTEGER,
+    0,
+  ]);
 
 /* istanbul ignore next */
 if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
--- 2020/day-23.spec.js
+++ 2020/day-23.spec.js
@@ -3,11 +3,12 @@ const { part1, part2, data } = require("./day-23");
 describe("Day 23", () => {
   describe("Part 1", () => {
     it("works for test data", () => {
-      expect(part1(data("test"))).toBe(undefined);
+      expect(part1(data("test"), 10)).toBe("92658374");
+      expect(part1(data("test"))).toBe("67384529");
     });
 
-    it.skip("works for real data", () => {
-      expect(part1(data())).toBe(undefined);
+    it("works for real data", () => {
+      expect(part1(data())).toBe("43769582");
     });
   });
 
