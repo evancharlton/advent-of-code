ff8b1b01c355c651e08a59cd970f3b27f3a29470
Evan Charlton <evancharlton@gmail.com>
2020-12-23 20:19:02 +0100

    chore: Add comments to Day 13
    I really struggled with this one, so walk myself through it to make sure
    that I understand what's going on.

--- 2020/day-13.js
+++ 2020/day-13.js
@@ -32,16 +32,31 @@ const part2 = ({ schedules }) => {
           return { step: busID, time: busID };
         }
 
+        // Check if the bus is out of service. If so, the multiplier doesn't
+        // change.
         if (Number.isNaN(busID)) {
           return acc;
         }
 
         let { step, time } = acc;
+        // Advance the clock <step> iterations at a time to find the next
+        // time that the bus will be <i> ticks from leaving the station. This
+        // will result in the stair-step pattern.
         while ((time + i) % busID !== 0) {
           time += step;
         }
 
-        return { step: step * busID, time };
+        // We can now multiply the step by the ID (schedule) of the bus. This
+        // works because this bus (& all of its earlier peers) will only be in
+        // the desired configuration every <this> often. This lets us avoid
+        // inspecting the irrelevant "bad" time-steps for the next bus.
+        //
+        // Put another way: it doesn't matter if the next bus is in alignment
+        // if the previous buses (this one and all of its previous ones) aren't
+        // also in their specified position.
+        step *= busID;
+
+        return { step, time };
       },
       { step: 0, time: 0 }
     );
