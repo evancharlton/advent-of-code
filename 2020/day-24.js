4072e9a1b892f1d43eeb2dcdadab4c1e83cee15d
Evan Charlton <evancharlton@gmail.com>
2020-12-24 08:05:34 +0100

    Day 24 (part 1)

--- 2020/day-24.js
+++ 2020/day-24.js
@@ -1,13 +1,160 @@
 const data = (type = "") => {
-  const lines = require("./input")(__filename, "\n", type);
-  return lines;
+  return require("./input")(__filename, "\n", type).map((path) =>
+    getSteps(path)
+  );
 };
 
-const part1 = (lines) => {
-  return undefined;
+const getSteps = (path) => {
+  const steps = [];
+  for (let i = 0; i < path.length; i += 1) {
+    let step = path[i];
+    if (step === "e" || step === "w") {
+      steps.push(step);
+      continue;
+    }
+    if (step === "n" || step === "s") {
+      i += 1;
+      step += path[i];
+      steps.push(step);
+      continue;
+    }
+  }
+  return steps;
 };
 
-const part2 = (lines) => {
+const print = (tile = this) => {
+  return [
+    `#${tile.id} (${tile.color})`,
+    SIDES.map((side) => {
+      return `\t${side} => ${tile[side] !== undefined ? tile[side] : "<>"}`;
+    }),
+  ]
+    .flat()
+    .join("\n");
+};
+
+const FLIPS = {
+  black: "white",
+  white: "black",
+};
+
+const MIRROR_STEPS = {
+  e: "w",
+  w: "e",
+  se: "nw",
+  nw: "se",
+  sw: "ne",
+  ne: "sw",
+};
+
+const SIDES = ["e", "se", "sw", "w", "nw", "ne"];
+
+const createHive = () => {
+  let globalId = 0;
+  const hive = new Map();
+
+  const createTile = () => {
+    const created = {
+      id: globalId++,
+      color: "white",
+      toString: function () {
+        return print(this);
+      },
+    };
+    hive.set(created.id, created);
+    return created;
+  };
+
+  const surround = (tileId) => {
+    const tile = hive.get(tileId);
+
+    // Create new tiles as necessary
+    SIDES.forEach((side) => {
+      if (tile[side] === undefined) {
+        const created = createTile();
+        tile[side] = created.id;
+        created[MIRROR_STEPS[side]] = tile.id;
+      }
+    });
+
+    const neighborIds = SIDES.map((side) => tile[side]);
+    const neighbors = neighborIds.map((id) => hive.get(id));
+
+    if (neighbors.some((n) => !n)) {
+      console.warn(neighbors);
+      console.warn(tile.toString());
+      throw new Error("Missing neighbors");
+    }
+
+    const [e, se, sw, w, nw, ne] = neighbors;
+
+    // Now link them all up
+
+    e.sw = se.id;
+    e.nw = ne.id;
+
+    se.ne = e.id;
+    se.w = sw.id;
+
+    sw.e = se.id;
+    sw.nw = w.id;
+
+    w.se = sw.id;
+    w.ne = nw.id;
+
+    nw.sw = w.id;
+    nw.e = ne.id;
+
+    ne.w = nw.id;
+    ne.se = e.id;
+
+    return tile;
+  };
+
+  const start = createTile();
+  const edgeIds = [start.id];
+
+  for (let i = 0; i < 20; i += 1) {
+    const addedIds = new Set();
+    while (edgeIds.length) {
+      const edgeTileId = edgeIds.shift();
+      const surroundedTile = surround(edgeTileId);
+      SIDES.forEach((side) => {
+        addedIds.add(surroundedTile[side]);
+      });
+    }
+    edgeIds.push(...addedIds);
+  }
+
+  return hive;
+};
+
+const part1 = (paths) => {
+  const hive = createHive();
+
+  paths.forEach((steps, i) => {
+    let current = hive.get(0);
+    steps.forEach((step, j) => {
+      current = hive.get(current[step]);
+      if (!current) {
+        console.warn(`Path #${i}, step #${j}`);
+        throw new Error("Walked off the hive!");
+      }
+    });
+    current.color = FLIPS[current.color];
+  });
+
+  let blackTiles = 0;
+  hive.forEach(({ color }) => {
+    if (color === "black") {
+      blackTiles += 1;
+    }
+  });
+
+  return blackTiles;
+};
+
+const part2 = (input) => {
   return undefined;
 };
 
