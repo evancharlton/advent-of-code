449df46d7296db9202bc0f056566739bb50f6eda
Evan Charlton <evancharlton@gmail.com>
2020-12-25 07:16:27 +0100

    Day 25 (part 2)

--- 2020/day-25.js
+++ 2020/day-25.js
@@ -31,18 +31,12 @@ const part1 = ([cardPublicKey, doorPublicKey]) => {
   return value;
 };
 
-const part2 = (lines) => {
-  return undefined;
-};
-
 /* istanbul ignore next */
 if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
   console.log(`Part 1:`, part1(data(process.argv[2] || "")));
-  console.log(`Part 2:`, part2(data(process.argv[2] || "")));
 }
 
 module.exports = {
   data,
   part1,
-  part2,
 };
--- 2020/day-25.spec.js
+++ 2020/day-25.spec.js
@@ -1,4 +1,4 @@
-const { part1, part2, data } = require("./day-25");
+const { part1, data } = require("./day-25");
 
 describe("Day 25", () => {
   describe("Part 1", () => {
@@ -8,16 +8,6 @@ describe("Day 25", () => {
 
     it("works for real data", () => {
       expect(part1(data())).toBe(711945);
-    });
-  });
-
-  describe("Part 2", () => {
-    it("works for test data", () => {
-      expect(part2(data("test"))).toBe(undefined);
-    });
-
-    it.skip("works for real data", () => {
-      expect(part2(data())).toBe(undefined);
     });
   });
 });
