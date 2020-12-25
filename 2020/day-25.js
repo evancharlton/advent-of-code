e8090dfbbb9e78652ec4f3c5e6fd77a1f2373d0d
Evan Charlton <evancharlton@gmail.com>
2020-12-25 07:16:00 +0100

    Day 25 (part 1)

--- 2020/day-25.js
+++ 2020/day-25.js
@@ -1,10 +1,34 @@
 const data = (type = "") => {
-  const lines = require("./input")(__filename, "\n", type);
-  return lines;
+  return require("./input")(__filename, "\n", type).map(Number);
 };
 
-const part1 = (lines) => {
-  return undefined;
+const DIVISOR = 20201227;
+
+const part1 = ([cardPublicKey, doorPublicKey]) => {
+  let subjectNumber = 7;
+  let value = 1;
+  let i = 0;
+  for (i = 1; true; i += 1) {
+    if (i > 2_000_000) {
+      console.error("Stopping after too many trials");
+      throw new Error("Limit reached");
+    }
+    value *= subjectNumber;
+    value %= DIVISOR;
+
+    if (value === cardPublicKey) {
+      break;
+    }
+  }
+
+  const loops = i;
+  value = 1;
+  subjectNumber = doorPublicKey;
+  for (i = 0; i < loops; i += 1) {
+    value *= subjectNumber;
+    value %= DIVISOR;
+  }
+  return value;
 };
 
 const part2 = (lines) => {
--- 2020/day-25.spec.js
+++ 2020/day-25.spec.js
@@ -3,11 +3,11 @@ const { part1, part2, data } = require("./day-25");
 describe("Day 25", () => {
   describe("Part 1", () => {
     it("works for test data", () => {
-      expect(part1(data("test"))).toBe(undefined);
+      expect(part1(data("test"))).toBe(14897079);
     });
 
-    it.skip("works for real data", () => {
-      expect(part1(data())).toBe(undefined);
+    it("works for real data", () => {
+      expect(part1(data())).toBe(711945);
     });
   });
 
