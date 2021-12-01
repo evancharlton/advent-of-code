4efa69f41b4052fcc197c573b2b2d4dd7645c46a
Evan Charlton <evancharlton@gmail.com>
2021-12-01 06:15:53 +0100

    Boilerplate for Day 2

---
+++ 2021/day-02.spec.js
@@ -0,0 +1,25 @@
+const { part1, part2, data } = require("./day-02");
+
+describe("Day 01", () => {
+  test("Part 1", () => {
+    // test for simple data
+    // expect(part1([])).toEqual(undefined);
+
+    // test for test data
+    expect(part1(data("test"))).toEqual(undefined);
+
+    // test for real data
+    expect(part1(data())).toEqual(undefined);
+  });
+
+  test("Part 2", () => {
+    // test for simple data
+    // expect(part1([])).toEqual(undefined);
+
+    // test for test data
+    expect(part2(data("test"))).toEqual(undefined);
+
+    // test for real data
+    expect(part2(data())).toEqual(undefined);
+  });
+});
