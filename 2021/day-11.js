f0110d20de1256d453262a34acb2fe47dd2dd480
Evan Charlton <evancharlton@gmail.com>
2021-12-11 07:41:24 +0100

    Day 11 (part 1)

---
+++ 2021/day-11.js
@@ -0,0 +1,201 @@
+const data = (type = "") => {
+  return require("./input")(__filename, "\n", type);
+};
+
+const DEBUG = false;
+
+const sortKeys = (a, b) => {
+  const [xa, ya] = a.split(",").map((v) => +v);
+  const [xb, yb] = b.split(",").map((v) => +v);
+
+  if (yb !== ya) {
+    return ya - yb;
+  }
+  return xa - xb;
+};
+
+const parse = (lines) =>
+  lines.map((line) =>
+    line
+      .trim()
+      .split("")
+      .map((v) => +v)
+  );
+
+const createBoard = (data) => {
+  const board = {};
+  for (let y = 0; y < data.length; y += 1) {
+    for (let x = 0; x < data[y].length; x += 1) {
+      board[`${x},${y}`] = data[y][x];
+    }
+  }
+  return board;
+};
+
+const tick = (current) => {
+  const next = {};
+
+  const flasherKeys = [];
+  const flashedKeys = new Set();
+
+  if (DEBUG) {
+    console.log("===== (current)");
+    console.log(printBoard(current));
+  }
+
+  // Do a pass over the grid, increment everyone.
+  Object.entries(current).forEach(([key, level]) => {
+    next[key] = level + 1;
+    if (level === 9) {
+      flasherKeys.push(key);
+    }
+  });
+
+  if (DEBUG) {
+    console.log("===== (after inc)");
+    console.log(printBoard(next));
+  }
+
+  // Do more passes and propagate the flashes
+  let LIMIT = 10000;
+  while (flasherKeys.length > 0 && LIMIT-- > 0) {
+    const key = flasherKeys.shift();
+    if (flashedKeys.has(key)) {
+      // console.log(`${key} has already flashed this cycle`);
+      continue;
+    }
+    // console.log(`${key} is flashing`);
+    flashedKeys.add(key);
+    const [x, y] = key.split(",").map((v) => +v);
+
+    // Increment the neighbors.
+    const neighbors = [
+      [x, y - 1],
+      [x + 1, y - 1],
+      [x + 1, y],
+      [x + 1, y + 1],
+      [x, y + 1],
+      [x - 1, y + 1],
+      [x - 1, y],
+      [x - 1, y - 1],
+    ]
+      .map(([x1, y1]) => `${x1},${y1}`)
+      .filter((n) => next[n] !== undefined)
+      .sort(sortKeys);
+
+    // console.log(`${key}'s neighbors are: ${neighbors.join(" ")}`);
+
+    neighbors.forEach((neighborKey) => {
+      const level = next[neighborKey];
+      const nextLevel = level + 1;
+      // console.log(`  --> ${neighborKey}: ${level} -> ${nextLevel}`);
+      next[neighborKey] = nextLevel;
+      if (next[neighborKey] > 9) {
+        if (!flasherKeys.includes(neighborKey)) {
+          flasherKeys.push(neighborKey);
+        }
+      }
+    });
+  }
+
+  if (DEBUG) {
+    console.log("===== (after flashing)");
+    console.log(printBoard(next));
+  }
+
+  // Reset anyone who has flashed.
+  Object.entries(next)
+    .filter(([_key, level]) => level > 9)
+    .forEach(([key]) => {
+      // console.log(`Resetting ${key} to 0 after it flashed.`);
+      next[key] = 0;
+    });
+
+  if (DEBUG) {
+    console.log("===== (after resets)");
+    console.log(printBoard(next));
+  }
+
+  Object.entries(next).forEach(([key, value]) => {
+    if (Number.isNaN(value)) {
+      throw new Error(`Bad value @ ${key}`);
+    }
+  });
+
+  const flashes = Object.values(next).filter((v) => v === 0).length;
+  if (DEBUG) {
+    console.log(`       --> ${flashes} flashes`);
+  }
+
+  // console.log([...flashedKeys].sort(sortKeys));
+
+  // if (Object.values(next).filter((v) => v === 0).length !== flashedKeys.size) {
+  //   throw new Error(
+  //     `${Object.values(next).filter((v) => v === 0).length} !== ${
+  //       flashedKeys.size
+  //     }`
+  //   );
+  // }
+
+  return { next, flashes };
+};
+
+const printBoard = (board) => {
+  const sortedKeys = Object.keys(board).sort(sortKeys);
+
+  return sortedKeys.reduce((output, key, i, keys) => {
+    if (i > 0) {
+      const [_, y] = key.split(",");
+      const [_0, y0] = keys[i - 1].split(",");
+      if (y !== y0) {
+        output += "\n";
+      }
+    }
+    output += `${board[key]}`;
+    return output;
+  }, "");
+};
+
+const part1 = (data, steps = 100) => {
+  const board = createBoard(parse(data));
+  let current = board;
+  let totalFlashes = 0;
+  for (let i = 0; i < steps; i += 1) {
+    const { next, flashes } = tick(current);
+    totalFlashes += flashes;
+    current = next;
+  }
+  return totalFlashes;
+};
+
+const part2 = (data) => {
+  return data;
+};
+
+/* istanbul ignore next */
+if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
+  const board = createBoard(parse(data("test-step-30")));
+  let current = board;
+  let totalFlashes = 0;
+  for (let i = 30; i <= 40; i += 1) {
+    console.log(`After step ${i}: (taking step # ${i + 1})`);
+    console.log(printBoard(current));
+    const { next, flashes } = tick(current);
+    totalFlashes += flashes;
+    console.log(`    Step ${i + 1} complete`);
+    console.log(`    --> ${totalFlashes} flashes in total`);
+    current = next;
+  }
+  // console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+  // console.log(`Part 2:`, part2(data(process.argv[2] || "")));
+}
+
+module.exports = {
+  data,
+  part1,
+  part2,
+  parse,
+  createBoard,
+  tick,
+  printBoard,
+};
---
+++ 2021/day-11.spec.js
@@ -0,0 +1,193 @@
+const { basename } = require("path");
+const DAY = basename(__filename).replace(".spec.js", "");
+const {
+  part1,
+  part2,
+  data,
+  tick,
+  createBoard,
+  parse,
+  printBoard,
+} = require(`./${DAY}`);
+
+const TEST = "test data";
+const PROD = "real data";
+
+describe(DAY.replace("-", " "), () => {
+  test("tick", () => {
+    const { next, flashes } = tick(
+      createBoard(
+        parse(
+          `
+11111
+19991
+19191
+19991
+11111
+    `
+            .trim()
+            .split("\n")
+        )
+      )
+    );
+    expect(printBoard(next)).toEqual(
+      `
+34543
+40004
+50005
+40004
+34543
+`.trim()
+    );
+
+    expect(flashes).toEqual(9);
+  });
+
+  test("Missing flash", () => {
+    const { next, flashes } = tick(createBoard(parse(data("test-step-2"))));
+    console.log(printBoard(next));
+    expect(flashes).toEqual(45);
+  });
+
+  test.only("steps", () => {
+    const board = createBoard(parse(data("test")));
+    let current = board;
+
+    let totalFlashes = 0;
+    for (let i = 0; i < 100; i += 1) {
+      const { next, flashes } = tick(current);
+      const step = i + 1;
+      totalFlashes += flashes;
+      if (STAGES[step]) {
+        try {
+          expect(printBoard(next)).toEqual(STAGES[step].trim());
+        } catch (ex) {
+          console.log(`Failed from ${i} -> ${step}`);
+          console.log(`At step ${i}:\n${printBoard(current)}`);
+          console.log(`After step ${step}:\n${printBoard(next)}`);
+          throw ex;
+        }
+      }
+      current = next;
+    }
+  });
+
+  test.only("Part 1", () => {
+    // test for simple data
+    // expect(part1([])).toEqual(undefined);
+
+    // test for test data
+    expect(part1(data("test"), 1)).toEqual(0);
+    expect(part1(data("test"), 2)).toEqual(35);
+    expect(part1(data("test-step-2"), 1)).toEqual(45);
+    expect(part1(data("test"), 3)).toEqual(35 + 45);
+    expect(part1(data("test"), 10)).toEqual(204);
+
+    expect(part1(data("test"))).toEqual(1656);
+
+    // test for real data
+    expect(part1(data())).toEqual(1599);
+  });
+
+  test("Part 2", () => {
+    // test for simple data
+    // expect(part2([])).toEqual(undefined);
+
+    // test for test data
+    expect(part2(data("test"))).toEqual(TEST);
+
+    // test for real data
+    expect(part2(data())).toEqual(PROD);
+  });
+});
+
+const STAGES = {
+  [1]: `
+6594254334
+3856965822
+6375667284
+7252447257
+7468496589
+5278635756
+3287952832
+7993992245
+5957959665
+6394862637`,
+  [2]: `
+8807476555
+5089087054
+8597889608
+8485769600
+8700908800
+6600088989
+6800005943
+0000007456
+9000000876
+8700006848`,
+  [10]: `
+0481112976
+0031112009
+0041112504
+0081111406
+0099111306
+0093511233
+0442361130
+5532252350
+0532250600
+0032240000`,
+  [20]: `
+3936556452
+5686556806
+4496555690
+4448655580
+4456865570
+5680086577
+7000009896
+0000000344
+6000000364
+4600009543`,
+  [30]: `
+0643334118
+4253334611
+3374333458
+2225333337
+2229333338
+2276733333
+2754574565
+5544458511
+9444447111
+7944446119`,
+  [40]: `
+6211111981
+0421111119
+0042111115
+0003111115
+0003111116
+0065611111
+0532351111
+3322234597
+2222222976
+2222222762`,
+  [50]: `
+9655556447
+4865556805
+4486555690
+4458655580
+4574865570
+5700086566
+6000009887
+8000000533
+6800000633
+5680000538`,
+  [100]: `
+0397666866
+0749766918
+0053976933
+0004297822
+0004229892
+0053222877
+0532222966
+9322228966
+7922286866
+6789998766`,
+};
