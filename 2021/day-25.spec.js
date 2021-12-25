fc45675d0f43047d46d754e0e0a2b96881064d73
Evan Charlton <evancharlton@gmail.com>
2021-12-25 06:41:36 +0100

    Day 25 (part 2)

--- 2021/day-25.spec.js
+++ 2021/day-25.spec.js
@@ -1,6 +1,6 @@
 const { basename } = require("path");
 const DAY = basename(__filename).replace(".spec.js", "");
-const { part1, part2, data, step, print } = require(`./${DAY}`);
+const { part1, data, step, print } = require(`./${DAY}`);
 
 const TEST = "test data";
 const PROD = "real data";
@@ -42,13 +42,5 @@ v.....>
 
     // test for real data
     expect(part1(data())).toEqual(351);
-  });
-
-  test.skip("Part 2", () => {
-    // test for test data
-    expect(part2(data("test"))).toEqual(TEST);
-
-    // test for real data
-    expect(part2(data())).toEqual(PROD);
   });
 });
