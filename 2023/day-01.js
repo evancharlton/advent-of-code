2f76c022b40870322f7102fd4e02d795fd9bd737
Evan Charlton <evancharlton@gmail.com>
2023-12-01 06:32:44 +0100

    2023 - day 1

---
+++ 2023/day-01.js
@@ -0,0 +1,81 @@
+const data = (type = "") => {
+  return require("./input")(__filename, "\n", type);
+};
+
+const part1 = (lines) => {
+  return lines
+    .filter((line) => !!line)
+    .map((line) => {
+      try {
+        const [_, first] = line.match(/^[a-z]*([\d])/);
+        const [__, last] = line.match(/([\d])[a-z]*$/);
+        return +[first, last].join("");
+      } catch (e) {
+        console.log(line, e);
+        throw e;
+      }
+    })
+    .reduce((acc, v) => acc + v);
+};
+
+const part2 = (lines) => {
+  const NUMS = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  return lines
+    .filter((line) => !!line)
+    .map((line) => {
+      const first = (() => {
+        for (let i = 0; i < line.length; i += 1) {
+          if (!Number.isNaN(+line[i])) {
+            return +line[i];
+          }
+
+          const sub = line.substring(i);
+          for (const key in NUMS) {
+            if (sub.startsWith(key)) {
+              return NUMS[key];
+            }
+          }
+        }
+        console.warn(`No start digit for ${line}`);
+      })();
+      const last = (() => {
+        for (let i = line.length - 1; i >= 0; i -= 1) {
+          if (!Number.isNaN(+line[i])) {
+            return +line[i];
+          }
+
+          const sub = line.substring(i);
+          for (const key in NUMS) {
+            if (sub.startsWith(key)) {
+              return NUMS[key];
+            }
+          }
+        }
+        console.warn(`No end digit for ${line}`);
+      })();
+      return +[first, last].join("");
+    })
+    .reduce((acc, v) => acc + v);
+};
+
+if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
+  console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+  console.log(`Part 2:`, part2(data(process.argv[2] || "")));
+}
+
+module.exports = {
+  data,
+  part1,
+  part2,
+};
---
+++ 2023/day-01.spec.js
@@ -0,0 +1,21 @@
+const { basename } = require("path");
+const DAY = basename(__filename).replace(".spec.js", "");
+const { part1, part2, data } = require(`./${DAY}`);
+
+describe(DAY.replace("-", " "), () => {
+  test.skip("Part 1", () => {
+    // test for test data
+    expect(part1(data("test"))).toEqual(142);
+
+    // test for real data
+    expect(part1(data())).toEqual(55538);
+  });
+
+  test("Part 2", () => {
+    // test for test data
+    expect(part2(data("test"))).toEqual(281);
+
+    // test for real data
+    expect(part2(data())).toEqual(54875);
+  });
+});
