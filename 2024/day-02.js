66074d7582b43c6dc8a641b0f246191ca48fef2f
Evan Charlton <evancharlton@gmail.com>
2024-12-02 06:21:41 +0100

    2024 / day 02 (both parts)

--- 2024/day-02.js
+++ 2024/day-02.js
@@ -1,13 +1,42 @@
 const data = (type = "") => {
-  return require("./input")(__filename, { type, trim: true });
+  return require("./input")(__filename, { type, trim: true }).map((line) =>
+    line.split(/\s+/).map((v) => +v)
+  );
+};
+
+const validate = (report) => {
+  const direction = report[0] - report[1] > 0;
+  for (let i = 1; i < report.length; i += 1) {
+    const prev = report[i - 1];
+    const curr = report[i];
+    const diff = prev - curr;
+    const absDiff = Math.abs(diff);
+    if (absDiff <= 0 || absDiff > 3) {
+      return false;
+    }
+
+    if (diff > 0 !== direction) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const part1 = (lines) => {
-  return undefined;
+  return lines.filter(validate).length;
 };
 
 const part2 = (lines) => {
-  return undefined;
+  return lines.filter((report) => {
+    for (let i = 0; i < report.length; i += 1) {
+      const copy = [...report];
+      copy.splice(i, 1);
+      if (validate(copy)) {
+        return true;
+      }
+    }
+    return false;
+  }).length;
 };
 
 if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
