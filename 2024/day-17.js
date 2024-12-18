7bd8392e08fcd01d6be345a94e95ffb8c1932ab2
Evan Charlton <evancharlton@gmail.com>
2024-12-18 05:59:10 +0100

    2024 / day 17 (part 1)

---
+++ 2024/day-17.js
@@ -0,0 +1,22 @@
+const data = (type = "") => {
+  return require("./input")(__filename, { type, trim: true });
+};
+
+const part1 = (lines) => {
+  return undefined;
+};
+
+const part2 = (lines) => {
+  return undefined;
+};
+
+if (process.argv.includes(__filename.replace(/\.[jt]s$/, ""))) {
+  console.log(`Part 1:`, part1(data(process.argv[2] || "")));
+  console.log(`Part 2:`, part2(data(process.argv[2] || "")));
+}
+
+module.exports = {
+  data,
+  part1,
+  part2,
+};
---
+++ 2024/day-17.spec.js
@@ -0,0 +1,27 @@
+const { basename } = require("path");
+const DAY = basename(__filename).replace(".spec.js", "");
+const { part1, part2, data } = require(`./${DAY}`);
+
+describe(DAY.replace("-", " "), () => {
+  test("Part 1", () => {
+    const TEST_VALUE = "4,6,3,5,6,3,5,2,1,0";
+    const VALUE = "3,1,4,3,1,7,1,6,3";
+
+    // test for test data
+    expect(part1(data("test"))).toEqual(TEST_VALUE);
+
+    // test for real data
+    expect(part1(data())).toEqual(VALUE);
+  });
+
+  test("Part 2", () => {
+    const TEST_VALUE = undefined;
+    const VALUE = undefined;
+
+    // test for test data
+    expect(part2(data("test"))).toEqual(TEST_VALUE);
+
+    // test for real data
+    expect(part2(data())).toEqual(VALUE);
+  });
+});
