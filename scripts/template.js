26abd3f6fdf28414943ebfc16078fa0f2b1d3a9f
Evan Charlton <evancharlton@gmail.com>
2025-12-07 06:53:35 +0100

    Add some safety mechanisms for template
    I encountered an issue today: if `npm run today` is accidentally run
    after I started working, it'll wipe out any changes I've made -- oops!
    Thankfully I was able to hit "undo" in my editor and recover, but that's
    not a great approach.
    
    Instead, refuse to overwrite existing files, and only append to the
    input files.

--- scripts/template.js
+++ scripts/template.js
@@ -9,8 +9,8 @@ const [date, year] = (() => {
     process.argv[2] === "today"
       ? now.getDate()
       : process.argv[2] === undefined
-      ? now.getDate() + 1
-      : process.argv[2];
+        ? now.getDate() + 1
+        : process.argv[2];
 
   const date = `0${d}`.substr(-2);
   const year = process.argv[3] ?? String(now.getFullYear());
@@ -19,17 +19,22 @@ const [date, year] = (() => {
 
 const yearDir = path.resolve(path.join(__dirname, "..", year));
 
-fs.copyFileSync(
-  path.join(yearDir, "template"),
-  path.join(yearDir, `day-${date}.js`)
-);
+const safeCopy = (from, to) => {
+  if (!fs.existsSync(to)) {
+    fs.copyFileSync(from, to);
+  } else {
+    console.warn(`Refusing to overwrite ${to}`);
+  }
+};
 
-fs.copyFileSync(
+safeCopy(path.join(yearDir, "template"), path.join(yearDir, `day-${date}.js`));
+
+safeCopy(
   path.join(yearDir, "template.spec"),
   path.join(yearDir, `day-${date}.spec.js`)
 );
 
-fs.writeFileSync(path.join(yearDir, "input", `day-${date}.txt`), "");
-fs.writeFileSync(path.join(yearDir, "input", `day-${date}.test.txt`), "");
+fs.appendFileSync(path.join(yearDir, "input", `day-${date}.txt`), "");
+fs.appendFileSync(path.join(yearDir, "input", `day-${date}.test.txt`), "");
 
 execSync(`git add ${yearDir}/day-${date}.*`);
